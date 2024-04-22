b4777d0c92ea0957eac0e4a641f3aa7c600e7b01
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-22 22:30:04 +0300

    Adds search bar and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-
+import './App.css'
 //a89e7461
 
 const API_URL = 'https://www.omdbapi.com?apikey=a89e7461';
@@ -19,8 +19,23 @@ function App() {
   },[]);
 
   return (
-    <div>
-      App
+    <div className='app'>
+      <h1>FlixMovie</h1>
+      <div className='search'>
+        <input 
+        placeholder='Search for a movie'
+        type='text' 
+        value='Superman'
+        onChange={()=> {}}
+        />
+        <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      <div className='container'>
+
+      </div>
     </div>
   )
 }
